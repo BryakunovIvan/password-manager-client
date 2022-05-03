84ebe72b8dc075fe057b900db9db78ac711f25f5
Ivan Bryakunov <ivan.bryakunov@gmail.com>
2022-05-03 18:16:33 +0400

    Исправил правило eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,6 @@ module.exports = {
 		'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.js'] }],
 		'import/no-unresolved': 0,
 		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'import/prefer-default-export': 0,
 	},
 };
