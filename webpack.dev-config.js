1d47b501ded29a6b4613b711a59e26b45a5fd46a
Ivan Bryakunov <ivan.bryakunov@gmail.com>
2022-05-04 20:04:48 +0400

    Добавил сорс мапы в дев режим

--- webpack.dev-config.js
+++ webpack.dev-config.js
@@ -3,6 +3,7 @@ const commonConfig = require('./webpack.common-config');
 
 module.exports = merge(commonConfig, {
 	mode: 'development',
+	devtool: 'inline-source-map',
 	devServer: {
 		compress: true,
 		port: 9000,
