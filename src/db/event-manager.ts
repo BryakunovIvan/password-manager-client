45423a6817834b41a23f8c79a53e541b103a20ad
Ivan Bryakunov <ivan.bryakunov@gmail.com>
2022-05-06 20:40:35 +0400

    Рефакторинг функционала EventManager

--- src/db/event-manager.ts
+++ src/db/event-manager.ts
@@ -1,39 +1,60 @@
 type TSubscriberId = string | number;
+type TSubscribersFunc = Function;
 
 type TSubscriber = {
-	func: Function;
+	func: TSubscribersFunc;
 	_id: TSubscriberId
 }
 
 export class EventManager<T> {
-	subscribers: Array<TSubscriber> = [];
+	private subscribers: Array<TSubscriber> = [];
 
-	subscribe = (subscriber: Function, unicueId?: TSubscriberId) => {
-		let _id = unicueId;
-
-		if (_id && this.subscribers.some((sub) => sub._id === unicueId)) {
+	private subscribeFunctionWithId = (subscriber: TSubscribersFunc, id: TSubscriberId) => {
+		if (this.subscribers.some((sub) => sub._id === id)) {
 			this.subscribers = this.subscribers.map((sub) => {
-				if (sub._id === unicueId) {
-					return { func: subscriber, _id: unicueId };
+				if (sub._id === id) {
+					return { func: subscriber, _id: id };
 				}
 
 				return sub;
 			});
+		} else {
+			this.subscribers.push({ func: subscriber, _id: id });
 		}
 
-		if (!this.subscribers.some((sub) => sub.func === subscriber)) {
-			_id = _id || Math.random();
-			this.subscribers.push({ func: subscriber, _id });
-		}
-
-		return _id;
+		return id;
 	};
 
-	unsubscribe = (subscriber: Function | TSubscriberId) => {
+	private subscribeFunction = (subscriber: TSubscribersFunc) => {
+		const id = Math.random();
+		this.subscribers.push({ func: subscriber, _id: id });
+
+		return id;
+	};
+
+	private unsubscribeFunctionWithId = (id: TSubscriberId) => {
+		this.subscribers = this.subscribers.filter((sub) => sub._id !== id);
+	};
+
+	private unsubscribeFunction = (subscriber: TSubscribersFunc) => {
+		this.subscribers = this.subscribers.filter((sub) => sub.func !== subscriber);
+	};
+
+	subscribe = (subscriber: TSubscribersFunc, unicueId?: TSubscriberId) => {
+		if (unicueId) {
+			return this.subscribeFunctionWithId(subscriber, unicueId);
+		}
+
+		return this.subscribeFunction(subscriber);
+	};
+
+	unsubscribe = (subscriber: TSubscribersFunc | TSubscriberId) => {
 		if (typeof subscriber === 'function') {
-			this.subscribers = this.subscribers.filter((sub) => sub.func !== subscriber);
-		} else {
-			this.subscribers = this.subscribers.filter((sub) => sub._id !== subscriber);
+			this.unsubscribeFunction(subscriber);
+		}
+
+		if (typeof subscriber === 'number' || typeof subscriber === 'string') {
+			this.unsubscribeFunctionWithId(subscriber);
 		}
 	};
 
